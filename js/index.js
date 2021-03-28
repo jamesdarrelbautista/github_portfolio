da248185790a73d8a3e0ba71ae9c3e1e2ec1859e
James Darrel Bautista <jamesdarrel.bautista@g.msuiit.edu.ph>
2021-03-28 21:04:57 +0800

    Tested various jquery effects and edited some html

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
-    $("h1").hover(function () {
-        $(this).animate({left: '30px'},'fast');
-    },function () {
-        $(this).animate({left: '-=30px'},'fast');
+    // $("h1").hover(function () {
+    //     $(this).animate({left: '30px'},'fast');
+    // },function () {
+    //     $(this).animate({left: '-=30px'},'fast');
+    // });
+    $('h1').click(function () {
+        $("#pendleton").toggle(500);
+        $("#james").toggle(500);   
+    });
+
+    $('div:first').hover(function () {
+        $('div').removeClass("text-white bg-primary");
+    }, function () {
+        $('div').addClass("text-white bg-primary");
     });
 });
